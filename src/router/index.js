a20cd6ec0df2cfe3b9419299a5e3a5a78708423a
x-ves <56628945+x-ves@users.noreply.github.com>
2022-01-02 12:46:49 +0200

    404 page

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     // this generates a separate chunk (lineup.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "lineup" */ '../views/Lineup.vue')
+  },
+  {
+    path: "/:notFound(.*)",
+    name: "NotFound",
+    component: () => import(/* webpackChunkName: "notfound" */ '../views/NotFound.vue')
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="notFound">
+        <h1>ERROR 404</h1>
+        <p>Page not Found!</p>
+        <router-link to="/">Return</router-link>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    .notFound {
+        display: flex;
+        flex-direction: column;
+        margin: 5rem auto;
+
+        h1 {
+            font-size: $heading-font-size;
+            margin: 0.5rem;
+        }
+
+        p {
+            margin: 0.25rem;
+        }
+
+        a {
+            color: $main-text-color;
+            text-transform: uppercase;
+        }
+    }
+</style>
